863f9225f724bcb73c6ed9d0b2e4d4d0ef9cddd1
YouniS Bensalah <younis.bensalah@riseup.net>
2013-12-12 15:29:50 +0100

    added Vector::fromComponents(x,y,z) and Vector.xyz(x,y,z)

--- lib/vector.php
+++ lib/vector.php
@@ -9,13 +9,17 @@
 
 class Vector {
 
+    static public function fromComponents($x, $y, $z)
+    {
+        $vector = new Vector();
+        $vector->xyz($x, $y, $z);
+        return $vector;
+    }
+
     static public function fromPoints(Point $point_a, Point $point_b)
     {
         $vector = new Vector();
-        $vector->x($point_b->x() - $point_a->x());
-        $vector->y($point_b->y() - $point_a->y());
-        $vector->z($point_b->z() - $point_a->z());
-        $vector->refresh();
+        $vector->xyz($point_b->x()-$point_a->x(), $point_b->y()-$point_a->y(), $point_b->z()-$point_a->z());
         return $vector;
     }
 
@@ -27,6 +31,27 @@ class Vector {
     public function getNorm()
     {
         return $this->_norm;
+    }
+
+    public function xyz($x = null, $y = null, $z = null)
+    {
+        if (($x === null) && ($y === null) && ($z === null)) {
+            return array(
+                $this->_x,
+                $this->_y,
+                $this->_z
+            );
+        }
+        if ($x !== null) {
+            $this->_x = $x;
+        }
+        if ($y !== null) {
+            $this->_y = $y;
+        }
+        if ($z !== null) {
+            $this->_z = $z;
+        }
+        $this->refresh();
     }
 
     /**
