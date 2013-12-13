43d0f8be760276805c2e5614b074a9e36d1e91a0
YouniS Bensalah <younis.bensalah@riseup.net>
2013-12-13 07:50:39 +0100

    plan is awesome

--- lib/plan.php
+++ lib/plan.php
@@ -9,19 +9,43 @@
 
 class Plan {
 
+    public function __construct()
+    {
+        $this->_n = new Vector();
+        $this->_u = new Vector();
+        $this->_v = new Vector();
+        $this->_p = new Vector();
+    }
+
     static public function fromPoints($point_a, $point_b, $point_c)
     {
-
+        $plan = new Plan();
+        // n: AB x AC
+        // u: AB
+        // v: AC
+        // p: OA
     }
 
-    static public function fromNormalVectorPoint($n_vector, $point)
+    static public function fromNormalVectorPointVector($normal_vector, $point_vector)
     {
 
     }
 
-    static public function fromDirectionalVectorsPoint($dir_vector_u, $dir_vector_v, $point)
+    static public function fromDirectionalVectorsPointVector($dir_vector_u, $dir_vector_v, $point)
     {
 
     }
+
+    /** Normal vector. */
+    protected $_n;
+
+    /** First directional vector. */
+    protected $_u;
+
+    /** Second directional vector. */
+    protected $_v;
+
+    /** Vector to Point. */
+    protected $_p;
 
 }
